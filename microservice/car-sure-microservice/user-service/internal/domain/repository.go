d50d5bc5a2d958c3ce1aceb77c52bfc5d1682a94
artitaya <artitaya2466@gmail.com>
2025-08-04 15:53:40 +0700

    delete project

--- microservice/car-sure-microservice/user-service/internal/domain/repository.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type UserRepository interface {
-	Create(user *User) error
-	FindByEmail(email string) (*User, error)
-}
--- microservice/car-sure-microservice/user-service/internal/utils/error.go
+++
@@ -1,5 +0,0 @@
-package utils
-
-import "errors"
-
-var ErrNotFound = errors.New("record not found")
