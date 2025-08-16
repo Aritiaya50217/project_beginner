d935777e241990623b9b0d69ceec4f127cb8f39a
artitaya <artitaya2466@gmail.com>
2025-08-16 13:13:51 +0700

    delete notification service

--- microservice/smart-stock-trading-platform/kafka-init/create_topics.sh
+++ microservice/smart-stock-trading-platform/kafka-init/create_topics.sh
@@ -4,11 +4,11 @@ set -e
 echo "Waiting for Kafka broker to be ready..."
 cub kafka-ready -b broker1:9092 1 20
 
-echo "Creating topic orders..."
+echo "Creating topic order-created..."
 kafka-topics --create \
-  --topic orders \
+  --topic order-created \
   --bootstrap-server broker1:9092 \
   --partitions 1 \
   --replication-factor 1 || echo "Topic already exists"
 
-echo "Topic orders is ready"
+echo "Topic order-created is ready"
