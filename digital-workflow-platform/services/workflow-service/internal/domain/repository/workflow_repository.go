6bb3c836071ec3f84554abd6b56c01d1e64d234d
artitaya <artitaya2466@gmail.com>
2025-12-23 18:54:41 +0700

    no message

--- digital-workflow-platform/services/workflow-service/internal/domain/repository/workflow_repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "workflow-service/internal/domain/entity"
-
-type WorkflowRepository interface {
-	Create(workflow *entity.Workflow) error
-	FindByID(id string) (*entity.Workflow, error)
-	Update(workflow *entity.Workflow) error
-}
